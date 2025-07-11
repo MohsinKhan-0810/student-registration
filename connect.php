d98e437581f5cd6bb0b6af62876bfdd81243d3f6
Mohsin Khan <mohsink0810@gmail.com>
2025-07-12 04:11:10 +0530

    Initial commit - Student Registration System

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php 
+
+$host = "localhost"; 
+$users = "root"; 
+$pass = ""; 
+$db = "login"; 
+$conn = new mysqli($host,$users,$pass,$db); 
+
+if($conn->connect_error) {
+    echo "Failed to connect DB".$conn->connect_error;
+}
+?>
